bd0491cb2ef666c5f7d77f49516753136af89c7f
PavelStojanov <pavel.stojanov@students.finki.ukim.mk>
2025-10-27 22:54:12 +0100

    fixed

--- src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
@@ -1,4 +1,34 @@
 package mk.ukim.finki.wp.lab.bootstrap;
 
+import jakarta.annotation.PostConstruct;
+import mk.ukim.finki.wp.lab.model.Book;
+import mk.ukim.finki.wp.lab.model.BookReservation;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class DataHolder {
+    public static List<Book> books = null;
+    public static List<BookReservation> reservations = null;
+
+    @PostConstruct
+    public void init() {
+        books = new ArrayList<>();
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+        books.add(new Book("Harry Potter", "Fantasy", 9));
+
+        reservations = new ArrayList<>();
+
+    }
 }
--- src/main/java/mk/ukim/finki/wp/lab/model/BookReservation.java
+++ src/main/java/mk/ukim/finki/wp/lab/model/BookReservation.java
@@ -1,4 +1,13 @@
 package mk.ukim.finki.wp.lab.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class BookReservation {
+    private String bookTitle;
+    private String readerName;
+    private String readerAddress;
+    private Long numberOfCopies;
 }
