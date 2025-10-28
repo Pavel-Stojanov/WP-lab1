07f96160ca7939ea0e5837f43e67eba135f6813e
PavelStojanov <pavel.stojanov@students.finki.ukim.mk>
2025-10-28 01:18:43 +0100

    repos

---
+++ src/main/java/mk/ukim/finki/wp/lab/repository/impl/InMemoryBookReservationRepository.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.lab.repository.impl;
+
+import mk.ukim.finki.wp.lab.bootstrap.DataHolder;
+import mk.ukim.finki.wp.lab.model.BookReservation;
+import mk.ukim.finki.wp.lab.repository.BookReservationRepository;
+
+public class InMemoryBookReservationRepository implements BookReservationRepository {
+    @Override
+    public BookReservation save(BookReservation reservation) {
+        DataHolder.reservations.add(reservation);
+        return reservation;
+    }
+}
